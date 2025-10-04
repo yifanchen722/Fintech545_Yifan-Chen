58c867a7a921a3fb45011ddd8f44a0379911c6d1
yifanchen722 <yc722@duke.edu>
2025-10-04 03:15:34 -0400

    8.3

---
+++ test_8.3.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import t
+
+df = np.loadtxt("/Users/nico/Desktop/input/test7_2.csv", delimiter=",", skiprows=1)
+
+nu, mu, sigma = t.fit(df)
+
+K = 100000
+
+samples = np.random.uniform(low=0, high=1, size=K)
+for i in range(K):
+    samples[i] = t.ppf(samples[i], nu, loc=mu, scale=sigma)
+
+# empirical distribution
+unique_sorted = np.sort(np.unique(samples))
+arr = np.empty(len(unique_sorted))
+for i in range(len(unique_sorted)):
+    arr[i] = np.sum(samples == unique_sorted[i]) / len(samples)
+for i in range(1, len(unique_sorted)):
+    arr[i] = arr[i - 1] + arr[i]
+
+# calculate VAR and ES
+var95 = 0
+for i in range(len(arr)):
+    if arr[i] >= 0.05:
+        var95 = unique_sorted[i - 1]
+        break
+
+mean_diff = abs(mu - var95)
+print("VAR=", abs(var95))
+print("mean_diff=", mean_diff)
+
+output_path = "/Users/nico/Desktop/output_8.3.csv"
+pd.DataFrame({"VaR": [abs(var95)], "Diff from Mean": [mean_diff]}).to_csv(
+    output_path, index=False, header=True
+)
