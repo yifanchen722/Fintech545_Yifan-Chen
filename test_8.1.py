86cd4ff316604a2a58e31272ae2ff1e7d3a2a093
yifanchen722 <yc722@duke.edu>
2025-10-04 02:34:03 -0400

    8.1

--- test_8.1.py
+++ test_8.1.py
@@ -12,3 +12,8 @@ mean_diff = abs(mean - VaR)
 
 print("VaR:\n", abs(VaR))
 print("Mean - VaR:\n", mean_diff)
+
+output_path = "/Users/nico/Desktop/output_8.1.csv"
+pd.DataFrame({"VaR": [abs(VaR)], "Diff from Mean": [mean_diff]}).to_csv(
+    output_path, index=False, header=True
+)
