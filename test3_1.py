81cef6f51227f7ce6f72b10c60a728770c4ac483
yifanchen722 <yc722@duke.edu>
2025-10-03 20:26:24 -0400

    3.1

--- test3_1.py
+++ test3_1.py
@@ -1,24 +1,14 @@
 import numpy as np
 import pandas as pd
+import test_lib
 
-with open("testout_1.3.csv", "r") as f:
-    lines = f.readlines()
+cov_matrix = np.loadtxt("../data/testout_1.3.csv", delimiter=",", skiprows=1)
 
-lines = lines[1:]
-data = [list(map(float, line.strip().split(","))) for line in lines]
-cov_matrix = np.array(data)
+C_hat = test_lib.get_near_psd(cov_matrix, 1e-6)
 
+print(C_hat)
 
-def near_psd(A, epsilon=1e-8):
-    B = (A + A.T) / 2
-    eigvals, eigvecs = np.linalg.eigh(B)
-    eigvals[eigvals < epsilon] = epsilon
-    psd_matrix = eigvecs @ np.diag(eigvals) @ np.linalg.inv(eigvecs)
-    psd_matrix = (psd_matrix + psd_matrix.T) / 2
-    return psd_matrix
+# eigenval, eigenvec = np.linalg.eig(cov_matrix)
 
-
-near_cov = near_psd(cov_matrix)
-near_cov_df = pd.DataFrame(near_cov, columns=["x1", "x2", "x3", "x4", "x5"])
-near_cov_df.to_csv("testout_1.3_nearPSD.csv", float_format="%.15f", index=False)
-print(near_cov_df)
+# print(eigenval)
+# print(eigenvec[0, 0])
