8807d632b8d7d38fd0ddb25b0a31e14caffee9cf
yifanchen722 <yc722@duke.edu>
2025-10-04 00:42:15 -0400

    update

---
+++ test_5.4.py
@@ -0,0 +1,44 @@
+import numpy as np
+import test_lib
+import pandas as pd
+
+C = np.loadtxt("/Users/nico/Desktop/input/test5_3.csv", delimiter=",", skiprows=1)
+var = np.diag(np.diag(C))
+C = np.linalg.inv(np.sqrt(var)) @ C @ np.linalg.inv(np.sqrt(var))
+
+maxiter = 100000
+tol = 1e-7
+
+deltaS = np.zeros((C.shape[0], C.shape[1]))
+Y = C
+gamma_1 = np.finfo(np.float64).max
+gamma_2 = np.finfo(np.float64).max
+R = np.zeros((C.shape[0], C.shape[1]))
+X = np.zeros((C.shape[0], C.shape[1]))
+
+
+for k in range(1, maxiter + 1):
+    gamma_1 = gamma_2
+    R = Y - deltaS
+    X = test_lib.PSA(R)
+    deltaS = X - R
+    Y = test_lib.PUA(X)
+    gamma_2 = test_lib.Frobeniusnorm(Y - C)
+    print("iter:", k, "gamma_2:", gamma_2)
+    if abs(gamma_1 - gamma_2) <= tol:
+        break
+
+Y = np.sqrt(var) @ Y @ np.sqrt(var)
+print(Y)
+
+K = 100000
+L = np.linalg.cholesky(Y)
+M = np.random.normal(loc=0, scale=1, size=(Y.shape[0], K))
+
+V = (L @ M).T
+sample_cov = np.cov(V, rowvar=False)
+
+np.set_printoptions(precision=15, suppress=True)
+
+output_path = "/Users/nico/Desktop/output_5.4.csv"
+pd.DataFrame(sample_cov).to_csv(output_path, index=False, header=False)
--- test_lib.py
+++ test_lib.py
@@ -27,3 +27,42 @@ def get_near_psd(dt, eps):
     C = B @ B.T
 
     return np.sqrt(var) @ C @ np.sqrt(var)
+
+
+def Frobeniusnorm(A):
+    return np.sum(np.square(A))
+
+
+def Frobeniusnorm_W(A, W):
+    return np.sum(np.square(np.linalg.sqrtm(W) @ A @ np.linalg.sqrtm(W)))
+
+
+def PUA(A):
+    I = np.eye(A.shape[0])
+    theta = np.diag(A - I)
+    return A - np.diag(theta)
+
+
+def PUA_W(A, W):
+    I = np.eye(A.shape[0])
+    theta = np.linalg.inv(np.square(np.linalg.inv(W))) @ np.diag(A - I)
+    return A - np.linalg.inv(W) @ np.diag(theta) @ np.linalg.inv(W)
+
+
+def APLUS(A):
+    eigenval, eigenvec = np.linalg.eig(A)
+    eigenval_p = np.maximum(eigenval, 1e-8)
+    lam = np.diag(eigenval_p)
+    return eigenvec @ lam @ eigenvec.T
+
+
+def PSA(A):
+    return APLUS(A)
+
+
+def PSA_W(A, W):
+    return (
+        np.linalg.sqrtm(np.linalg.inv(W))
+        @ APLUS(np.linalg.sqrtm(W) @ A @ np.linalg.sqrtm(W))
+        @ np.linalg.sqrtm(np.linalg.inv(W))
+    )
