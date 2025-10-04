edb3427dd6319c18b1c073451a24a4f618013cf9
yifanchen722 <yc722@duke.edu>
2025-10-04 01:44:20 -0400

    5.5

---
+++ test_5.5.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from scipy.linalg import sqrtm
+import test_lib
+
+cov_matrix = np.loadtxt(
+    "/Users/nico/Desktop/input/test5_2.csv", delimiter=",", skiprows=1
+)
+
+
+eigenval, eigenvec = np.linalg.eig(cov_matrix)
+
+eigenindex = np.argsort(eigenval)[::-1]
+
+eigenval_sort = eigenval[eigenindex]
+eigenvec_sort = eigenvec[:, eigenindex]
+
+PCT = 0
+
+for i in range(0, len(eigenval_sort)):
+    if np.sum(eigenval_sort[0 : i + 1]) / np.sum(eigenval_sort) > 0.99:
+        PCT = i + 1
+        break
+print(PCT)
+
+S = eigenvec_sort[:, :PCT]
+lam = sqrtm(np.diag(eigenval_sort[:PCT]))
+L = B = S @ lam
+
+K = 100000
+M = np.random.normal(loc=0, scale=1, size=(L.shape[1], K))
+
+X = (L @ M).T
+sample_cov = np.cov(X, rowvar=False)
+
+np.set_printoptions(precision=15, suppress=True)
+
+output_path = "/Users/nico/Desktop/output_5.5.csv"
+pd.DataFrame(sample_cov).to_csv(output_path, index=False, header=False)
