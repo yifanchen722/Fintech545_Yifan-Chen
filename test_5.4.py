9e3511cfb772c74542f08683fc92bbb18a382632
yifanchen722 <yc722@duke.edu>
2025-10-16 22:45:38 -0400

    5.4

--- test_5.4.py
+++ test_5.4.py
@@ -2,33 +2,12 @@ import numpy as np
 import test_lib
 import pandas as pd
 
-C = np.loadtxt("/Users/nico/Desktop/test5_3.csv", delimiter=",", skiprows=1)
-var = np.diag(np.diag(C))
-C = np.linalg.inv(np.sqrt(var)) @ C @ np.linalg.inv(np.sqrt(var))
+data_path = "../data/test5_3.csv"
 
 maxiter = 100000
 tol = 1e-10
 
-deltaS = np.zeros((C.shape[0], C.shape[1]))
-Y = C
-gamma_1 = np.finfo(np.float64).max
-gamma_2 = np.finfo(np.float64).max
-R = np.zeros((C.shape[0], C.shape[1]))
-X = np.zeros((C.shape[0], C.shape[1]))
-
-
-for k in range(1, maxiter + 1):
-    gamma_1 = gamma_2
-    R = Y - deltaS
-    X = test_lib.PSA(R)
-    deltaS = X - R
-    Y = test_lib.PUA(X)
-    gamma_2 = test_lib.Frobeniusnorm(Y - C)
-    print("iter:", k, "gamma_2:", gamma_2)
-    if abs(gamma_1 - gamma_2) <= tol:
-        break
-
-Y = np.sqrt(var) @ Y @ np.sqrt(var)
+Y = test_lib.higham(data_path, maxiter, tol)
 print(Y)
 
 K = 100000
