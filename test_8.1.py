c8a31af9b3d45793d7168ca17348742c7dd8147d
yifanchen722 <yc722@duke.edu>
2025-10-16 22:51:29 -0400

    8.1

--- test_8.1.py
+++ test_8.1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import norm
 
-data = np.loadtxt("/Users/nico/Desktop/input/test7_1.csv", delimiter=",", skiprows=1)
+data = np.loadtxt("../data/test7_1.csv", delimiter=",", skiprows=1)
 
 mean = np.mean(data, axis=0)
 sd = np.std(data, axis=0, ddof=1)
@@ -13,7 +13,7 @@ mean_diff = abs(mean - VaR)
 print("VaR:\n", abs(VaR))
 print("Mean - VaR:\n", mean_diff)
 
-output_path = "/Users/nico/Desktop/output_8.1.csv"
+output_path = "/Users/nico/Desktop/output.csv"
 pd.DataFrame({"VaR": [abs(VaR)], "Diff from Mean": [mean_diff]}).to_csv(
     output_path, index=False, header=True
 )
