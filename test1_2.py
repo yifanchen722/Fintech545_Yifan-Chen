be2166f99fcc64219de2c8e32a8c1a65b75d348f
yifanchen722 <yc722@duke.edu>
2025-10-19 15:13:28 -0400

    Update test1_2.py

--- test1_2.py
+++ test1_2.py
@@ -3,6 +3,5 @@ import pandas as pd
 df = pd.read_csv("../data/test1.csv")
 df_clean = df.dropna()
 corr_matrix = df_clean.corr()
-pd.options.display.float_format = "{:.16f}".format
+
 print(corr_matrix)
-corr_matrix.to_csv("testout.csv", index=False)
