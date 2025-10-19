3f61876d0b2976cd18da464d1f140535e8acd614
yifanchen722 <yc722@duke.edu>
2025-10-19 15:21:00 -0400

    1.4

--- test1_4.py
+++ test1_4.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("test1.csv")
+df = pd.read_csv("../data/test1.csv")
 pairwise_corr = df.corr()
-pd.options.display.float_format = "{:.16f}".format
+
 print(pairwise_corr)
-pairwise_corr.to_csv("testout.csv", index=False)
