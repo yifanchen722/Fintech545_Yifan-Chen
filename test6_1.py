20ffeb5370c7303cab05e302ef4dd47d171b0fcb
yifanchen722 <yc722@duke.edu>
2025-10-19 17:42:56 -0400

    6.1

--- test6_1.py
+++ test6_1.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 data = pd.read_csv("../data/test6.csv", parse_dates=["Date"], index_col="Date")
 returns = data.pct_change().dropna()
-returns.to_csv("arithmetic_returns.csv")
+
 print(returns.head())
